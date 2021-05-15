627eb6e5acfecd8a4e9381c1dfc2c8aa8c94d83e
Julian Vega <ingjulianvega@gmail.com>
2021-05-15 15:49:06 -0500

    :zap: Eureka.Closes #28

---
+++ src/main/java/ingjulianvega/ximic/msscasubodycheck/configuration/LocalDiscovery.java
@@ -0,0 +1,9 @@
+package ingjulianvega.ximic.msscasubodycheck.configuration;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local-discovery")
+@EnableDiscoveryClient
+public class LocalDiscovery {
+}
